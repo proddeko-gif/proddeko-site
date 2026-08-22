7d97e216a7b372aa5675896b25aed8f4ff6eae3d
proddeko-gif <proddeko@gmail.com>
2026-08-23 00:25:38 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -93,7 +93,7 @@ export default function App(){
 
       <div className="mt-4 flex flex-wrap gap-x-5 gap-y-2 text-sm">
         <a
-       href="/mentions-legaux.html"
+       href="/mentions-legales.html"
           className="transition hover:text-white"
         >
           Mentions légales
